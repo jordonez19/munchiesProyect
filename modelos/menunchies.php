52b4903494ca71073a014ca30a88b0a87fd6bfca
javier <javier19pedraza@gmail.com>
2021-01-27 22:01:33 -0500

    uploading status

---
+++ modelos/menunchies.php
@@ -0,0 +1,6 @@
+
+
+
+<div class="container__menunchies">
+    <img class="menunchies" src="https://image.freepik.com/vector-gratis/dibujado-mano-elementos-carne-letras-banner-menu-carnes-restaurante_80590-2358.jpg" alt="">
+</div>
